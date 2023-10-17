033d21e0043c5ccd7c6152819d36f9ab038d59a4
Paras Jain <64364245+ParasJain013@users.noreply.github.com>
2023-10-17 23:54:50 +0530

    NavBar Completed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import './Navbar.scss';
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="../../public/img/en.png" alt="" />
+            {/* <img src="../../public/img/indiaImg.jpg" alt="" /> */}
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1"> Women</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2"> Men</Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className="link" to="/">ShopUp</Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link className="link">About</Link>
+          </div>
+          <div className="item">
+            <Link className="link">Contact</Link>
+          </div>
+          <div className="item">
+            <Link className="link">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon/>
+            <PersonOutlineOutlinedIcon/>
+            <FavoriteBorderOutlinedIcon/>
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon/>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
